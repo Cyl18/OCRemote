e914f7f48c5ac0c59046169c31f4750d49680d5d
ResView <lyyxwangrunze@163.com>
2024-01-29 06:34:44 +0800

    request logic change

--- OCRemoteServer/Controller/OCReportController.cs
+++ OCRemoteServer/Controller/OCReportController.cs
@@ -1,4 +1,7 @@
 ﻿using System.Collections.Concurrent;
+using System.Numerics;
+using System.Text.Json;
+using GammaLibrary.Extensions;
 using Microsoft.AspNetCore.Mvc;
 
 namespace OCRemoteServer.Controller
@@ -12,7 +15,7 @@ namespace OCRemoteServer.Controller
         {
             using var s = HttpContext.Request.Body;
             using var sr = new StreamReader(s);
-            
+
             RemoteManager.Callback(await sr.ReadToEndAsync());
             return Ok();
         }
@@ -20,24 +23,17 @@ namespace OCRemoteServer.Controller
         [HttpGet]
         public async Task<IActionResult> GetEvent()
         {
-            var time = DateTime.Now;
-            while (DateTime.Now - time < TimeSpan.FromSeconds(5))
+            Dictionary<int, string> list = new();
+            while (RemoteManager.commandQueue.TryDequeue(out var x))
             {
-                CancellationToken cancellationToken = HttpContext.RequestAborted;
-                if (cancellationToken.IsCancellationRequested)
-                {
-                    break;
-                }
-
-                if (RemoteManager.commandQueue.TryDequeue(out var x))
-                {
-                    return Content(x);
-                }
-
-                SpinWait.SpinUntil(() => RemoteManager.commandQueue.Count > 0, 5);
+                list.Add(x.Item1, x.Item2);
             }
-
-            return NoContent();
+            if (list.IsEmpty())
+            {
+                return NoContent();
+            }
+            var json = JsonSerializer.Serialize(list);
+            return Content(json);
         }
     }
 }
